ee4d15a1500c51da3c64786ec68078f5049de8e5
itztli <itztli@gmail.com>
2013-06-20 21:17:02 -0500

    Creating first view

--- roomates/roomates/settings.py
+++ roomates/roomates/settings.py
@@ -111,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/home/vdelaluz/local/repository/git/roomates/roomates/templates'
 )
 
 INSTALLED_APPS = (
